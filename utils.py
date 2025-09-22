73c5e98f936997ae3414fa1d71a395fb864ad0ec
Jamison Kelly <jamison.m.kelly@gmail.com>
2025-09-22 16:27:24 -0400

    Run all black.

--- utils.py
+++ utils.py
@@ -1,4 +1,3 @@
 class InvalidArgumentError(Exception):
     def __init__(self, arg, msg):
-        super().__init__(
-            f'The `{arg}` (type `{type(arg)}`) is invalid. {msg}')
+        super().__init__(f"The `{arg}` (type `{type(arg)}`) is invalid. {msg}")
